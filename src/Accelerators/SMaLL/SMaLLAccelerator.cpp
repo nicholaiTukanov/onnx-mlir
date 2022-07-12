afdf13f88a63cfc42fda8f0087a0772c5340213c
nicholaiTukanov <nicholaitukanov@gmail.com>
2022-07-12 15:48:44 -0400

    use NNPA as boiler plate for SMaLL MLIR dialect

--- src/Accelerators/SMaLL/SMaLLAccelerator.cpp
+++ src/Accelerators/SMaLL/SMaLLAccelerator.cpp
@@ -2,91 +2,95 @@
 //  * SPDX-License-Identifier: Apache-2.0
 //  */
 
-// //
-// // Add accelerator support for the IBM Telum processor.
-// //
-// //===----------------------------------------------------------------------===//
+// Add accelerator support for the SMaLL ML framework
+//
+//===----------------------------------------------------------------------===//
 
-// #include "mlir/Conversion/LLVMCommon/TypeConverter.h"
-// #include "mlir/IR/BuiltinTypes.h"
-// #include "mlir/Transforms/Passes.h"
-// #include "llvm/Support/Debug.h"
+#include "mlir/Conversion/LLVMCommon/TypeConverter.h"
+#include "mlir/IR/BuiltinTypes.h"
+#include "mlir/Transforms/Passes.h"
+#include "llvm/Support/Debug.h"
 
-// #include "src/Accelerators/NNPA/Compiler/NNPACompilerUtils.hpp"
-// #include "src/Accelerators/NNPA/Conversion/ZHighToZLow/ZHighToZLow.hpp"
-// #include "src/Accelerators/NNPA/Conversion/ZLowToLLVM/ZLowToLLVM.hpp"
-// #include "src/Accelerators/NNPA/Dialect/ZHigh/ZHighOps.hpp"
-// #include "src/Accelerators/NNPA/Dialect/ZLow/ZLowOps.hpp"
-// #include "src/Accelerators/NNPA/NNPAAccelerator.hpp"
-// #include "src/Accelerators/NNPA/Pass/NNPAPasses.hpp"
-// #include "src/Compiler/CompilerOptions.hpp"
-// #include "zdnn.h"
+#include "src/Accelerators/SMaLL/Compiler/NNPACompilerUtils.hpp"
+#include "src/Accelerators/SMaLL/Conversion/SMaLLToTiNY/SMaLLToTiNY.hpp"
+#include "src/Accelerators/SMaLL/Conversion/TiNYToSMaLL/TiNYToSMaLL.hpp"
+#include "src/Accelerators/SMaLL/Dialect/SMaLL/SMaLLOps.hpp"
+#include "src/Accelerators/SMaLL/SMaLLAccelerator.hpp"
+#include "src/Accelerators/SMaLL/Pass/SMaLLPasses.hpp"
+#include "src/Compiler/CompilerOptions.hpp"
+#include "small.h"
 
-// #include <memory>
+#include <memory>
 
-// #define DEBUG_TYPE "NNPAAccelerator"
+#define DEBUG_TYPE "SMaLLAccelerator"
 
-// extern llvm::cl::OptionCategory OMNNPAPassOptions;
+extern llvm::cl::OptionCategory OMNNPAPassOptions;
 
-// namespace onnx_mlir {
-// namespace accel {
+namespace onnx_mlir {
+namespace accel {
 
-// Accelerator *createNNPA() { return NNPAAccelerator::getInstance(); }
+Accelerator *createSMaLL() { return SMaLLAccelerator::getInstance(); }
 
-// NNPAAccelerator *NNPAAccelerator::instance = nullptr;
+SMaLLAccelerator *SMaLLAccelerator::instance = nullptr;
 
-// NNPAAccelerator *NNPAAccelerator::getInstance() {
-//   if (instance == nullptr)
-//     instance = new NNPAAccelerator();
-//   return instance;
-// }
+SMaLLAccelerator *SMaLLAccelerator::getInstance() {
+  if (instance == nullptr)
+    instance = new SMaLLAccelerator();
+  return instance;
+}
 
-// NNPAAccelerator::NNPAAccelerator() : Accelerator(Accelerator::Kind::NNPA) {
-//   LLVM_DEBUG(llvm::dbgs() << "Creating an NNPA accelerator\n");
-//   acceleratorTargets.push_back(this);
-//   // Order is important! libRuntimeNNPA depends on libzdnn
-//   addCompilerConfig(CCM_SHARED_LIB_DEPS, {"RuntimeNNPA", "zdnn"});
-// };
+SMaLLAccelerator::SMaLLAccelerator() : Accelerator(Accelerator::Kind::NNPA) {
+  LLVM_DEBUG(llvm::dbgs() << "Creating an SMaLL accelerator\n");
+  acceleratorTargets.push_back(this);
+  // Order is important! libRuntimeNNPA depends on libzdnn
+  // TODO: fix dependencies (we know small lib is needed but what else?)
+  addCompilerConfig(CCM_SHARED_LIB_DEPS, {"small"});
+};
 
-// NNPAAccelerator::~NNPAAccelerator() { delete instance; }
+SMaLLAccelerator::~SMaLLAccelerator() { delete instance; }
 
-// uint64_t NNPAAccelerator::getVersionNumber() const { return ZDNN_VERNUM; }
+uint64_t SMaLLAccelerator::getVersionNumber() const { return SMALL_VERNUM; }
 
-// void NNPAAccelerator::getOrLoadDialects(mlir::MLIRContext &context) const {
-//   LLVM_DEBUG(llvm::dbgs() << "Loading dialects for NNPA accelerator\n");
-//   context.getOrLoadDialect<zhigh::ZHighDialect>();
-//   context.getOrLoadDialect<zlow::ZLowDialect>();
-// }
+void SMaLLAccelerator::getOrLoadDialects(mlir::MLIRContext &context) const {
+  LLVM_DEBUG(llvm::dbgs() << "Loading dialects for SMaLL accelerator\n");
+  context.getOrLoadDialect<small::SMaLLDialect>();
+  context.getOrLoadDialect<tiny::TiNYDialect>();
+}
 
-// void NNPAAccelerator::addPasses(mlir::OwningOpRef<mlir::ModuleOp> &module,
-//     mlir::PassManager &pm,
-//     onnx_mlir::EmissionTargetType &emissionTarget) const {
-//   LLVM_DEBUG(llvm::dbgs() << "Adding passes for NNPA accelerator\n");
-//   addPassesNNPA(module, pm, emissionTarget);
-// }
+void SMaLLAccelerator::addPasses(mlir::OwningOpRef<mlir::ModuleOp> &module,
+    mlir::PassManager &pm,
+    onnx_mlir::EmissionTargetType &emissionTarget) const {
+  LLVM_DEBUG(llvm::dbgs() << "Adding passes for SMaLL accelerator\n");
+  addPassesSMaLL(module, pm, emissionTarget);
+}
 
-// void NNPAAccelerator::registerDialects(mlir::DialectRegistry &registry) const {
-//   LLVM_DEBUG(llvm::dbgs() << "Registering dialects for NNPA accelerator\n");
-//   registry.insert<zhigh::ZHighDialect>();
-//   registry.insert<zlow::ZLowDialect>();
-// }
+void SMaLLAccelerator::registerDialects(mlir::DialectRegistry &registry) const {
+  LLVM_DEBUG(llvm::dbgs() << "Registering dialects for SMaLL accelerator\n");
+  registry.insert<small::SMaLLDialect>();
+  registry.insert<tiny::TiNYDialect>();
+}
 
-// void NNPAAccelerator::initPasses(int optLevel) const {
-//   LLVM_DEBUG(llvm::dbgs() << "Initializing passes for NNPA accelerator\n");
+void SMaLLAccelerator::initPasses(int optLevel) const {
+  LLVM_DEBUG(llvm::dbgs() << "Initializing passes for SMaLL accelerator\n");
+
+  // this pass will convert ONNX models to the SMaLL ML framework
+  mlir::registerPass([]() -> std::unique_ptr<mlir::Pass> {
+    return onnx_mlir::createONNXToSMaLLPass();
+  });
+
+
+  mlir::registerPass([]() -> std::unique_ptr<mlir::Pass> {
+    return onnx_mlir::createRewriteONNXForSMaLLPass();
+  });
+
+  mlir::registerPass([]() -> std::unique_ptr<mlir::Pass> {
+    return onnx_mlir::tiny::createTiNYRewritePass();
+  });
+
+
+    // * commented out pass seem to be unnecessary
 //   mlir::registerPass([]() -> std::unique_ptr<mlir::Pass> {
-//     return onnx_mlir::createONNXToZHighPass();
-//   });
-
-//   mlir::registerPass([]() -> std::unique_ptr<mlir::Pass> {
-//     return onnx_mlir::createRewriteONNXForZHighPass();
-//   });
-
-//   mlir::registerPass([]() -> std::unique_ptr<mlir::Pass> {
-//     return onnx_mlir::zlow::createZLowRewritePass();
-//   });
-
-//   mlir::registerPass([]() -> std::unique_ptr<mlir::Pass> {
-//     return onnx_mlir::zlow::createZLowDummyOpForMultiDerefPass();
+//     return onnx_mlir::zlow::createTiNYDummyOpForMultiDerefPass();
 //   });
 
 //   mlir::registerPass(
@@ -99,42 +103,44 @@
 //   mlir::registerPass([]() -> std::unique_ptr<mlir::Pass> {
 //     return onnx_mlir::zhigh::createZHighLayoutPropagationPass();
 //   });
-// }
+}
 
-// mlir::MemRefType NNPAAccelerator::convertTensorTypeToMemRefType(
-//     const mlir::TensorType tensorType) const {
-//   assert(tensorType.hasRank() && "expected only ranked shapes");
-//   if (tensorType.cast<mlir::RankedTensorType>()
-//           .getEncoding()
-//           .dyn_cast_or_null<onnx_mlir::zhigh::ZTensorEncodingAttr>()) {
-//     onnx_mlir::zhigh::ZMemRefType zMemRefType =
-//         onnx_mlir::zhigh::convertZTensorToMemRefType(tensorType);
-//     return zMemRefType.value;
-//   }
-//   return nullptr;
-// }
+mlir::MemRefType SMaLLAccelerator::convertTensorTypeToMemRefType(
+    const mlir::TensorType tensorType) const {
+  assert(tensorType.hasRank() && "expected only ranked shapes");
+  if (tensorType.cast<mlir::RankedTensorType>()
+          .getEncoding()
+          .dyn_cast_or_null<onnx_mlir::zhigh::ZTensorEncodingAttr>()) {
+    onnx_mlir::zhigh::ZMemRefType zMemRefType =
+        onnx_mlir::zhigh::convertZTensorToMemRefType(tensorType);
+    return zMemRefType.value;
+  }
+  return nullptr;
+}
 
-// void NNPAAccelerator::conversionTargetONNXToKrnl(
-//     mlir::ConversionTarget &target) const {
-//   target.addLegalDialect<zlow::ZLowDialect>();
-// }
+// lowering?
+void SMaLLAccelerator::conversionTargetONNXToKrnl(
+    mlir::ConversionTarget &target) const {
+  target.addLegalDialect<tiny::TiNYDialect>();
+}
 
-// void NNPAAccelerator::rewritePatternONNXToKrnl(
-//     mlir::RewritePatternSet &patterns, mlir::TypeConverter &typeConverter,
-//     mlir::MLIRContext *ctx) const {
-//   onnx_mlir::zhigh::populateZHighToZLowConversionPattern(
-//       patterns, typeConverter, ctx);
-// }
 
-// void NNPAAccelerator::conversionTargetKrnlToLLVM(
-//     mlir::ConversionTarget &target) const {}
+void SMaLLAccelerator::rewritePatternONNXToKrnl(
+    mlir::RewritePatternSet &patterns, mlir::TypeConverter &typeConverter,
+    mlir::MLIRContext *ctx) const {
+  onnx_mlir::small::populateSMaLLToTiNYConversionPattern(
+      patterns, typeConverter, ctx);
+}
 
-// void NNPAAccelerator::rewritePatternKrnlToLLVM(
-//     mlir::RewritePatternSet &patterns, mlir::LLVMTypeConverter &typeConverter,
-//     mlir::MLIRContext *ctx) const {
-//   onnx_mlir::zlow::populateZLowToLLVMConversionPattern(
-//       patterns, typeConverter, ctx);
-// }
+void SMaLLAccelerator::conversionTargetKrnlToLLVM(
+    mlir::ConversionTarget &target) const {}
 
-// } // namespace accel
-// } // namespace onnx_mlir
+void SMaLLAccelerator::rewritePatternKrnlToLLVM(
+    mlir::RewritePatternSet &patterns, mlir::LLVMTypeConverter &typeConverter,
+    mlir::MLIRContext *ctx) const {
+  onnx_mlir::tiny::populateTiNYToLLVMConversionPattern(
+      patterns, typeConverter, ctx);
+}
+
+} // namespace accel
+} // namespace onnx_mlir
